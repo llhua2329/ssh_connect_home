4d20f3aacf40ef0581ac85c5f01f63c174bc0832
tbag <692603114@qq.com>
2018-08-25 21:02:41 +0800

    优化代码

--- client.go
+++ client.go
@@ -9,13 +9,14 @@ import (
 )
 
 func main() {
-	ip := flag.String("ip", "192.168.2.38", "remote ip")
+	ip := flag.String("ip", "127.0.0.1", "remote ip")
 	port := flag.Int("port", 50001, "remote port")
 	flag.Parse()
 
 	server_addr := *ip +":"+ strconv.Itoa(*port)
 	control_conn, err := net.Dial("tcp", server_addr)
 	if err != nil {
+		fmt.Println("connect server failed", err)
 		return
 	}
 	defer control_conn.Close()
@@ -26,20 +27,16 @@ func main() {
 	ctrl.send = make(chan []byte)
 	go ctrl.Read()
 	go ctrl.Write()
-	Run(ctrl, server_addr)
-}
 
-func Run(ctrl connection, server_addr string) {
-	heartbeat := make([]byte, 1)
-	heartbeat[0] = 0xFE
-	byte := make([]byte, 1)
+	recv := make([]byte, 1)
 	tick := time.Tick(5 * time.Second)
 	for {
 		select {
 		case <- tick: // 定时发送心跳
+			heartbeat := []byte{0xFE}
 			ctrl.send <- heartbeat
-		case byte = <-ctrl.recv:
-			if byte[0] == 0xFF { // 发起新的ssh连接
+		case recv = <-ctrl.recv:
+			if recv[0] == 0xFF { // 发起新的ssh连接
 				fmt.Println(time.Now(), "accept now ssh")
 				go newSsh(server_addr)
 			}
@@ -56,7 +53,7 @@ func newSsh(server_info string) {
 	}
 	defer remote.Close()
 	fmt.Println(time.Now(), "send local ssh")
-	local, err := net.Dial("tcp", "192.168.1.200:22")
+	local, err := net.Dial("tcp", "127.0.0.1:22")
 	if err != nil {
 		fmt.Println("ssh info connect to 22:", err)
 		return
--- server.go
+++ server.go
@@ -6,81 +6,79 @@ import (
 	"flag"
 	"strconv"
 	"time"
+	"os"
 )
+
+type myListener struct {
+	conn chan net.Conn
+}
+
+func (l *myListener) Init(port int) {
+	lister, err := net.Listen("tcp", "0.0.0.0:" + strconv.Itoa(port))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(10)
+	}
+	defer lister.Close()
+	for {
+		conn, err := lister.Accept()
+		if err != nil {
+			fmt.Println("accept err", err)
+			break
+		}
+		fmt.Println("accept new ")
+		l.conn <- conn
+		fmt.Println("accept new connect ")
+	}
+}
 
 func main() {
 	user_port := flag.Int("user_port", 50000, "user connet port")
 	home_port := flag.Int("home_port", 50001, "home connect_port")
 	flag.Parse()
-	listen_user, err := net.Listen("tcp", "0.0.0.0:"+strconv.Itoa(*user_port))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer listen_user.Close()
 
-	listen_home, err := net.Listen("tcp", "0.0.0.0:"+strconv.Itoa(*home_port))
-	if err != nil {
-		fmt.Println(time.Now(), err)
-		return
-	}
-	defer listen_home.Close()
-
-	control_conn, err := listen_home.Accept()
-	if err != nil {
-		fmt.Println(time.Now(), "control connect failed:", err)
-		return
-	}
-	defer control_conn.Close()
+	var home, user myListener
+	home.conn = make(chan net.Conn)
+	user.conn = make(chan net.Conn)
+	go home.Init(*home_port)
+	go user.Init(*user_port)
 
 	var ctrl connection
-	ctrl.conn = control_conn
 	ctrl.recv = make(chan []byte)
 	ctrl.send = make(chan []byte)
+
+	ctrl.conn = <- home.conn
+	fmt.Println("ctrl connection connected")
 	go ctrl.Read()
 	go ctrl.Write()
-	go func() { // 处理心跳
-		buf := make([]byte, 1)
-		heart := int64(time.Now().Unix())
-		tick := time.Tick(3 * time.Second)
-		for {
-			select {
-			case buf =<- ctrl.recv:
-				if (buf[0] == 0xFE) {
-					fmt.Println("heart from ctrl client")
-					heart = int64(time.Now().Unix())
-				}
-			case <-tick:
-				if int64(time.Now().Unix()) - heart > 20 {
-					fmt.Println("heart timeout")
-					ctrl.conn.Close()
-					return
-				}
+
+	heart := int64(time.Now().Unix())
+	tick := time.Tick(3 * time.Second)
+	recv := make([]byte ,1)
+	for {
+		select {
+		case user_conn := <- user.conn: // 处理新连接
+			go func() {
+				fmt.Println(time.Now(), "send new ssh notify to home client")
+				b := []byte{0xff}
+				ctrl.send <- b // 通知home 有新的ssh来连接
+
+				fmt.Println(time.Now(), "wait for home_conn")
+				home_conn := <- home.conn // 等待home来连接
+				fmt.Println(time.Now(), "accept home connected:", home_conn.RemoteAddr())
+				go SwapConn(user_conn, home_conn)
+			}()
+		case recv =<- ctrl.recv:
+			if (recv[0] == 0xFE) {
+				fmt.Println("heart from ctrl client")
+				heart = int64(time.Now().Unix())
+			}
+		case <-tick:
+			if int64(time.Now().Unix()) - heart > 20 {
+				fmt.Println("heart timeout")
+				ctrl.conn.Close()
+				return
 			}
 		}
-	}()
-
-	for {
-		user_conn, err := listen_user.Accept()
-		if err != nil {
-			fmt.Println("accept err", err)
-			continue
-		}
-
-		fmt.Println(time.Now(), "send new ssh notify to home client")
-		b := make([]byte, 1)
-		b[0] = 0xff
-		ctrl.send <- b // 通知home 有新的ssh来连接
-
-		fmt.Println(time.Now(), "wait for home_conn")
-		home_conn, err := listen_home.Accept() // 等待home来连接
-		if err != nil {
-			user_conn.Close()
-			fmt.Println(time.Now(), "accept home conn err:", err)
-			continue
-		}
-		fmt.Println(time.Now(), "accept home connected:", home_conn.RemoteAddr())
-
-		go SwapConn(user_conn, home_conn)
 	}
 }
